a4f42ffb89ee87ff225aaf95cb84fc1f38baf954
Nikita Begletskiy <steveefox50@icloud.com>
2020-05-20 11:23:20 +0300

    Task completed

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Colors {
+    case red
+    case yellow
+    case green
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var redColorView: UIView!
+    @IBOutlet weak var yellowColorView: UIView!
+    @IBOutlet weak var greenColorView: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    var colorIs: Colors = .red
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 15
+        startButton.setTitle("START", for: .normal)
+        
+        
+        redColorView.layer.cornerRadius = 50
+        redColorView.alpha = 0.3
+        
+        yellowColorView.layer.cornerRadius = 50
+        yellowColorView.alpha = 0.3
+        
+        greenColorView.layer.cornerRadius = 50
+        greenColorView.alpha = 0.3
+        
     }
 
 
+    @IBAction func changeColor(_ sender: UIButton) {
+        
+         startButton.setTitle("Next", for: .normal)
+        
+        
+        if colorIs == .red {
+            redColorView.alpha = 1
+            greenColorView.alpha = 0.3
+            colorIs = .yellow
+        } else if colorIs == .yellow {
+            yellowColorView.alpha = 1
+            redColorView.alpha = 0.3
+            colorIs = .green
+        } else if  colorIs == .green {
+            greenColorView.alpha = 1
+            yellowColorView.alpha = 0.3
+            colorIs = .red
+        } else {
+            return
+        }
+        
+        
+    }
+    
+    
 }
 
